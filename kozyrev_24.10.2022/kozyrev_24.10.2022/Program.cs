e0d90206afec98f862145339d8df6306a48d537d
kozyrev_evgeny <kozyrev2604@gmail.com>
2022-10-25 23:36:11 +0300

    first commit

---
+++ kozyrev_24.10.2022/kozyrev_24.10.2022/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        class Person
+        {
+            public string name = "hz";
+            public string Surname = "kz";
+            public int age;
+        }
+        static void Print(string result)
+        {
+            Console.WriteLine(result);
+        }
+        static void Main(string[] args)
+        {
+            Person josh = new Person();
+            josh.name = "Josh";
+            josh.Surname = "Boleslava";
+            josh.age = 15;
+            Print($"{josh.Surname} {josh.name} is {josh.age} years old");
+        }
+    }
+}
